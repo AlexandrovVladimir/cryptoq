0953f510ba4a46fa28da5b6952979eeddd36da87
A Z <beatable@mail.ru>
2018-11-05 23:33:11 +0300

    add to project

---
+++ app/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+  $("#anchor").on('click', function (e){
+  	e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("html,body").offset().top
+  	}, 750);
+  });
+
+  $('.tab-content-banners__info').on('click', function () {
+    $(this).next().toggle();
+    $(this).toggleClass('show');
+  });
+
+  $('.questions__text').on('click', function() {
+    $(this).next().toggle();
+  })
+});
