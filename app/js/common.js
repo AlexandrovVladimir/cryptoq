89578811bccb3064908cbafb8a063213a33b7bff
Alexey Zhukov <olexbeatable@gmail.com>
2018-11-07 11:07:44 +0200

    'finished'

--- app/js/common.js
+++ app/js/common.js
@@ -18,4 +18,29 @@ $(document).ready(function (){
   $('.navbar-mobile').on('click', function() {
     $(this).closest('.navbar-mobile').find('.navbar-collapse').toggleClass('show');
   });
+
+
+  $.widget( "custom.iconselectmenu", $.ui.selectmenu, {
+    _renderItem: function( ul, item ) {
+      var li = $( "<li>" ),
+          wrapper = $( "<div>", { text: item.label } );
+
+      if ( item.disabled ) {
+        li.addClass( "ui-state-disabled" );
+      }
+
+      $( "<span>", {
+        style: item.element.attr( "data-style" ),
+        "class": "ui-icon " + item.element.attr( "data-class" )
+      })
+          .appendTo( wrapper );
+
+      return li.append( wrapper ).appendTo( ul );
+    }
+  });
+
+  $( "#bitcoin-select" )
+      .iconselectmenu()
+      .iconselectmenu( "menuWidget" )
+      .addClass( "ui-menu-icons customicons" );
 });
